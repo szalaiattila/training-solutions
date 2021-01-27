207e515bcb70c735c88d6d2d88b6a32fabd6c426
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 00:40:04 +0100

    week06d01 feladaz

---
+++ src/main/java/week06d01/ListSelector.java
@@ -0,0 +1,10 @@
+public class ListSelector{
+    public static void main(String[] args) {
+        String s = "dog";
+        s.toUpperCase();
+        System.out.println(s);
+    }
+}
+
+
+        }
