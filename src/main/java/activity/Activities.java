5802da13d2e24921df0beaad7544a49258b15531
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-27 19:02:44 +0100

    activity projektfeladat

--- src/main/java/activity/Activities.java
+++ src/main/java/activity/Activities.java
@@ -1,1 +1,51 @@
+package activity;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Activities {
+    private List<Activity>  activities = new ArrayList<>();
+
+    public Activities(List<Activity> activities) {
+        this.activities = activities;
+    }
+    public void addActivity(Activity other) {
+        activities.add(other);
+    }
+    public List<Report> distancesByTypes() {
+        List<Report> reports = new ArrayList<>();
+        double fullDist = 0;
+        for (ActivityType activityType : ActivityType.values()) {
+            for(Activity activity : activities) {
+                if (activityType == activity.getType()) {
+                    fullDist += activity.getDistance();
+                }
+            }
+            reports.add(new Report(activityType,fullDist));
+        }
+
+        System.out.println(reports.toString());
+        return reports;
+    }
+
+
+    public int numberOfTrackActivities() {
+        int count = 0;
+        for (Activity activity : activities) {
+            if ( activity instanceof ActivityWithTrack) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public int numberOfWithoutTrackActivities() {
+        int count = 0;
+        for (Activity activity : activities) {
+            if ( activity instanceof ActivityWithoutTrack) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
