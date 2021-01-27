f38b1b411537dbcb8c008d4ada6d697ce302e8fa
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 00:15:43 +0100

    bankaccount feladat test

---
+++ src/test/java/algorithmscount/BankAccountConditionCounterTest.java
@@ -0,0 +1,23 @@
+package algorithmscount;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BankAccountConditionCounterTest {
+
+    @Test
+    public void testCountWithBalanceGreaterThan() {
+        List<BankAccount> bankAccounts = Arrays.asList(
+                new BankAccount("John Doe", "123", 100),
+                new BankAccount("Jane Doe", "123", 200),
+                new BankAccount("Joe Doe", "123", 300),
+                new BankAccount("Jennifer Doe", "123", 400));
+
+        assertEquals(3, new BankAccountConditionCounter().amountCounter(bankAccounts, 100));
+        assertEquals(0, new BankAccountConditionCounter().amountCounter(bankAccounts, 400));
+    }
+}
