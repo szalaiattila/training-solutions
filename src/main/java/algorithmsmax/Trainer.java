2d51dd16f698783ef4edb3257c471caf15466196
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 22:02:48 +0100

    algorithmsmax feladat

---
+++ src/main/java/algorithmsmax/Trainer.java
@@ -0,0 +1,24 @@
+package algorithmsmax;
+
+public class Trainer {
+    private String name;
+    private int age;
+
+    public Trainer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + age;
+    }
+}
