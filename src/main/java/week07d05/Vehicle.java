1ba41280e43ea2cccb438f377e1e2123dbcfa3b3
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 02:48:38 +0100

    week07d05 feladat

---
+++ src/main/java/week07d05/Vehicle.java
@@ -0,0 +1,14 @@
+package week07d05;
+
+import java.security.PublicKey;
+
+public class Vehicle {
+
+    public int getNumberOfGears() {
+        return 5;
+    }
+
+    public TransmissionType getTransmissionType() {
+        return TransmissionType.MANUAL;
+    }
+}
