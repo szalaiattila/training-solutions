c1490b6e3155175b14ee7c19255863271cc358cb
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 01:12:25 +0100

    week06d02 feladat test

---
+++ src/test/java/week06d02/StoreTest.java
@@ -0,0 +1,12 @@
+package week06d02;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StoreTest {
+
+    Store store = new Store(Arrays.asList(
+            new Product name "Kifli". Category, BAKE
+
+}
