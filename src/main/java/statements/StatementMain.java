1d81720b80e372dd0aad60765f174547975ec039
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 22:48:41 +0100

    statements feladat

---
+++ src/main/java/statements/StatementMain.java
@@ -0,0 +1,16 @@
+package statements;
+
+public class StatementMain {
+    public static void main(String[] args) {
+        int x = 5 + 6;
+        int y = 11 - x;
+        int z = 8;
+        boolean b = x > y;
+        boolean c = b || z > 5;
+        z++;
+        System.out.println(z);
+        System.out.println(y);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
