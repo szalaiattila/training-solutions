97054b12383bda3b2b9ff58146c704bf3b7bf170
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 14:32:29 +0100

    introjunit feladat

---
+++ src/main/java/introjunit/Gentleman.java
@@ -0,0 +1,11 @@
+package introjunit;
+
+public class Gentleman {
+    public String sayHello(String name) {
+        if (name == null) {
+            return "Hello Anonymous";
+        } else {
+            return "Hello" + " " + name;
+        }
+    }
+}
