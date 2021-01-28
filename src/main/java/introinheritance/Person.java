0d8a1fa4eca49a37fb58e1b93f8a6ca9942849bd
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 14:45:29 +0100

    introinheritance feladat

---
+++ src/main/java/introinheritance/Person.java
@@ -0,0 +1,23 @@
+package introinheritance;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public void migrate(String newAddress) {
+        this.address = newAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
