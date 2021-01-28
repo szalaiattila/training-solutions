0e36f026306b76ad3a7a55532542694e55f794ab
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 23:02:51 +0100

    packages feladat

---
+++ src/main/java/packages/main/MainProgram.java
@@ -0,0 +1,11 @@
+package packages.main;
+
+import packages.greetings.Greeter;
+
+public class MainProgram {
+    public static void main(String[] args) {
+
+        //packages.greetings.Greeter greeter = new Greeter();
+       new Greeter().sayHello();
+    }
+}
