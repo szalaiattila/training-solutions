7eb9540cc6ebe48bf2b0232154739446b826b9ae
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 02:42:53 +0100

    week07d04 feladat

---
+++ src/main/java/week07d04/Lab.java
@@ -0,0 +1,43 @@
+package week07d04;
+
+import java.time.LocalDate;
+
+public class Lab {
+
+    private String title;
+    private boolean completed;
+    private LocalDate completedAt;
+
+    public Lab(String title) {
+        this.title = title;
+        this.completed = false;
+    }
+
+    public Lab(String title, LocalDate completedAt) {
+        this.title = title;
+        this.completedAt = completedAt;
+        this.completed = true;
+    }
+
+    public void complete(LocalDate paraDate) {
+        this.completed = true;
+        this.completedAt = paraDate;
+    }
+
+    public void complete() {
+        this.completedAt = LocalDate.now();
+        this.completed = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public LocalDate getCompletedAt() {
+        return completedAt;
+    }
+}
