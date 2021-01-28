349f488e1bc1f2e429520afe010adc2f7959abd1
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-01-28 14:47:48 +0100

    introinheritance feladat

---
+++ src/main/java/introinheritance/BigBoss.java
@@ -0,0 +1,15 @@
+package introinheritance;
+
+public class BigBoss extends Boss{
+    private double bonus;
+    public BigBoss(String name, String address, double salary, int numberOfEmployees, double bonus) {
+        super(name, address, salary, numberOfEmployees);
+        this.bonus = bonus;
+    }
+    public int getNumberOfEmployees() {
+        return super.getNumberOfEmployees();
+    }
+    public double getSalary() {
+        return super.getSalary()  + bonus;
+    }
+}
