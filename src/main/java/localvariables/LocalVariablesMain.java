869d95c95c685f2e10b316eb2c330c46e4e2410d
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-02 23:29:58 +0100

    localvariables feladat

---
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,31 @@
+package localvariables;
+
+import java.time.LocalDateTime;
+
+public class LocalVariablesMain {
+
+    public static void main(String[] args) {
+
+        boolean b = false;
+        System.out.println(b);
+
+        int a = 2;
+        int i=3; int j = 4;
+        int k = i;
+
+        String s = "Hello World";
+        String t = s;
+
+        {
+            int x = 0;
+            System.out.println(a);
+        }
+
+        // System.out.println(x);
+
+
+
+
+
+    }
+}
