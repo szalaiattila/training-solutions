8bcd7498ac0a16f067334e0a815fde8e520e610b
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-03 00:27:38 +0100

    virtualmethod feladat

---
+++ src/main/java/virtualmethod/vehicle/Vehicle.java
@@ -0,0 +1,30 @@
+package virtualmethod.vehicle;
+
+public class Vehicle {
+
+    private int vehicleWeight;
+    public static final int PERSON_AVERAGE_WEIGHT = 75;
+
+    public Vehicle(int vehicleWeight) {
+        this.vehicleWeight = vehicleWeight;
+    }
+
+    public int getGrossLoad() {
+       return vehicleWeight +PERSON_AVERAGE_WEIGHT;
+    }
+
+    public int getVehicleWeight() {
+        return vehicleWeight;
+    }
+
+    public static int getPersonAverageWeight() {
+        return PERSON_AVERAGE_WEIGHT;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vehicleWeight=" + vehicleWeight +
+                '}';
+    }
+}
