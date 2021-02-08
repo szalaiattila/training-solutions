e7f98fe02c310f2dac6dfc4a723acfb68ff77bc6
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-09 00:10:14 +0100

    School records projektfeladat

---
+++ src/main/java/schoolrecords/Subject.java
@@ -0,0 +1,15 @@
+package schoolrecords;
+
+
+public class Subject {
+    private String subjectName;
+
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public Subject(String subjectName) {
+        this.subjectName = subjectName;
+    }
+}
