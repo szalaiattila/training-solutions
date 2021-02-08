8cc5ba7b339ae1489f6cfb80e03ab1b299b8bff4
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-08 23:48:21 +0100

    methoverloading feladat

---
+++ src/main/java/methoverloading/Pub.java
@@ -0,0 +1,27 @@
+package methoverloading;
+
+public class Pub {
+    private String name;
+    private Time openFrom;
+
+    public Pub(String name, int hours, int minutes) {
+        this.name = name;
+        openFrom = new Time(hours, minutes);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Time getOpenFrom() {
+        return openFrom;
+    }
+
+
+    @Override
+    public String toString() {
+        return name + ";" + openFrom;
+
+    }
+}
