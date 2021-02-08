29bfd65fb697afd148607201368d4f672a46236c
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-08 23:36:53 +0100

    methodparam feladat

---
+++ src/main/java/methodparam/ExtremValues.java
@@ -0,0 +1,19 @@
+package methodparam;
+
+public class ExtremValues {
+    private double min;
+    private double max;
+
+    public ExtremValues(double min, double max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public double getMax() {
+        return max;
+    }
+}
