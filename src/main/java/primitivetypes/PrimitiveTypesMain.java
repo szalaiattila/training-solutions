74ab9298d83daea28b05821aef08fe917d806338
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-08 23:52:26 +0100

    primitivetypes feladat

---
+++ src/main/java/primitivetypes/PrimitiveTypesMain.java
@@ -0,0 +1,10 @@
+package primitivetypes;
+
+public class PrimitiveTypesMain {
+    public static void main(String[] args) {
+        PrimitiveTypes primitiveTypes = new PrimitiveTypes();
+        String result = primitiveTypes.toBinaryString(8);
+        System.out.println(result);
+    }
+
+}
