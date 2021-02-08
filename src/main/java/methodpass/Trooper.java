ad5ec64440d942275eb3ec4b6da14980dffcc1da
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-08 23:40:50 +0100

    methodpass feladat

---
+++ src/main/java/methodpass/Trooper.java
@@ -0,0 +1,31 @@
+package methodpass;
+
+public class Trooper {
+    private String name;
+    private Position position;
+
+    public Trooper(String name) {
+        if ( name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name must not be empty.");
+        }
+        this.name = name;
+        this.position = new Position(0, 0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+    public void changePosition(Position target) {
+        if ( target == null) {
+            throw new IllegalArgumentException("Target can't be null.");
+        }
+        this.position = target;
+    }
+    public double distanceFrom(Position target) {
+      return this.position.distanceFrom(target);
+    }
+}
