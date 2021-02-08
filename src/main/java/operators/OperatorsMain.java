d7dc7967ab26f0fc5d0f25fb769416f1caedd981
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-08 23:25:39 +0100

    operators feladat

---
+++ src/main/java/operators/OperatorsMain.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class OperatorsMain {
+    public static void main(String[] args) {
+        Operators op = new Operators();
+        op.isEven(2);
+
+        System.out.println(op.multiplyByPowerOfTwo(16,2));
+    }
+
+
+}
