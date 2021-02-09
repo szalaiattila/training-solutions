078925d00df763ea0737fa09550673d146fecb0c
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-10 00:26:06 +0100

    Vezérlési szerkezetek és az elágazás

---
+++ src/main/java/controlselection/month/DayInMonth.java
@@ -0,0 +1,29 @@
+package controlselection.month;
+
+public class DayInMonth {
+    public int DayInMonth(int year, String month) {
+        switch(month) {
+            case "január":
+            case "március":
+            case "május":
+            case "július":
+            case "augusztus":
+            case "október":
+            case "december":
+                return 31;
+            case "április":
+            case "június":
+            case "szeptember":
+            case "november":
+                return 30;
+            case "február":
+                if(year % 4 == 0 && !(year % 100 == 0) || (year % 400 == 0)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            default:
+                throw new IllegalArgumentException("Invalid month: " + month);
+        }
+    }
+}
