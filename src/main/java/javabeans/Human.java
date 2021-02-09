827e9370c87df54a13c135c429de645ac4057d11
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-09 17:29:44 +0100

    Javabeans objektumok

---
+++ src/main/java/javabeans/Human.java
@@ -0,0 +1,31 @@
+package javabeans;
+
+public class Human {
+    private String name;
+    private double weight;
+    private int iq;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+}
