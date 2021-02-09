e85ddcdcf66c7b6a725323475cb42d061017ca77
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-10 00:17:31 +0100

    Ciklusok

---
+++ src/main/java/controliteration/dictionary/DictionaryItem.java
@@ -0,0 +1,28 @@
+package controliteration.dictionary;
+
+import java.util.List;
+
+public class DictionaryItem {
+    private String word;
+    private List<String> translations;
+
+    public DictionaryItem(String word, List<String> translations) {
+        this.word = word;
+        addTranslations(translations);
+    }
+    public void addTranslations(List<String> newTranslations) {
+        for (String newTranslation: newTranslations) {
+            if (!translations.contains(newTranslation)) {
+                translations.add(newTranslation);
+            }
+        }
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public List<String> getTranslations() {
+        return translations;
+    }
+}
