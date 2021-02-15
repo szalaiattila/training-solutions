7542dac82e88c43ec28a15dde0236f39315cb762
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-15 20:11:29 +0100

    Gyakorlati vizsgafeladat

---
+++ src/main/java/exam03/src/main/java/exam03/Histogram.java
@@ -0,0 +1,33 @@
+
+package exam03;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Histogram {
+
+    public String createHistogram(BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while((line=reader.readLine()) != null) {
+            for(int i = 0; i < Integer.parseInt(line); i++) {
+                sb.append("*");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        try(BufferedReader reader = Files.newBufferedReader(Path.of("src/test/resources/histogram.txt"))) {
+            Histogram histogram = new Histogram();
+            System.out.println(histogram.createHistogram(reader));
+        } catch(IOException e) {
+            throw new IllegalStateException("Can't read file!", e);
+        }
+    }
+
+
+}
