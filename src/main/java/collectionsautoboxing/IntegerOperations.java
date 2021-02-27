fd337636b815b49affbe7f4079e78a2973259bad
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-02-27 17:23:05 +0100

    Autoboxing használata kollekcióknál

---
+++ src/main/java/collectionsautoboxing/IntegerOperations.java
@@ -0,0 +1,29 @@
+package collectionsautoboxing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerOperations {
+    public List<Integer> createList(int... numbers) {
+        List<Integer> result = new ArrayList<>();
+        for(int i : numbers) {
+            result.add(i);
+        }
+        return result;
+    }
+    public int sumIntegerList(List<Integer> integerList) {
+        int sum = 0;
+        for(int i : integerList) {
+
+            sum+= i;
+        }
+        return sum;
+    }
+    public int sumIntegerObjects(Integer... integers) {
+        int sum = 0;
+        for(int i : integers) {
+            sum+=i;
+        }
+        return sum;
+    }
+}
