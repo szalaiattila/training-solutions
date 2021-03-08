66ace30c5a3169a6ef9e135e242ac4d2f5f37856
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-03-08 22:25:54 +0100

    week13d01 feladat

---
+++ src/main/java/week13d01/City.java
@@ -0,0 +1,19 @@
+package week13d01;
+
+public class City {
+    private String postalCode;
+    private String name;
+
+    public City(String postalCode, String name) {
+        this.postalCode = postalCode;
+        this.name = name;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
