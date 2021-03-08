5f8c32402755a9c5278fc350b1e67b33bfb9eb14
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-03-08 22:05:11 +0100

    week09d03 feladat

---
+++ src/test/java/week09d03/SantaClaus.java
@@ -0,0 +1,28 @@
+package week09d03;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SantaClaus {
+    private List<Person> persons = new ArrayList<>();
+
+    public SantaClaus(List<Person> persons) {
+        this.persons = persons;
+    }
+    public void getThroughChimneys() {
+        for (Person person : persons) {
+            person.setPresent();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SantaClaus{" +
+                "persons=" + persons +
+                '}';
+    }
+
+
+}
